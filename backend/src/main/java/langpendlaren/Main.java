c4b68ec71be2b5bc13ef1241746cb89bb846b8f4
ecspresso <emile@ecspreso.com>
2022-12-08 12:53:13 +0100

    En första fungerade push till trafikverket.

--- backend/src/main/java/langpendlaren/Main.java
+++ backend/src/main/java/langpendlaren/Main.java
@@ -1,7 +1,19 @@
 package langpendlaren;
 
+import langpendlaren.trafikverket.TrafikverketAPI;
+
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
-
+        TrafikverketAPI trafikverketAPI = new TrafikverketAPI();
+        try {
+            trafikverketAPI.post();
+        } catch(UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- backend/src/main/java/langpendlaren/http/Http.java
+++ backend/src/main/java/langpendlaren/http/Http.java
@@ -1,24 +1,43 @@
 package langpendlaren.http;
 
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public abstract class Http {
+    protected final HttpHost host;
+
+    protected Http(HttpHost host) {
+        this.host = host;
+    }
+
     protected void post(HttpPost httpPost) throws IOException {
+        StringBuffer result = new StringBuffer();
+
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            try (CloseableHttpResponse response1 = httpclient.execute(httpPost)) {
-                HttpEntity entity1 = response1.getEntity();
-                // do something useful with the response body
-                // and ensure it is fully consumed
+            try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
+                HttpEntity entity1 = response.getEntity();
+
+                int status = response.getStatusLine().getStatusCode();
+                System.out.println(status);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    result.append(line);
+                }
                 EntityUtils.consume(entity1);
             }
         }
+
+
     }
 }
