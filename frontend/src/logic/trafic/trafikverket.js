60824d2868542edeed686a848dbe8d941d92fc4f
Pakdel <habib.pakdel1121@gmail.com>
2022-12-29 22:58:46 +0100

    preDefined, search gobaly

--- frontend/src/logic/trafic/trafikverket.js
+++ frontend/src/logic/trafic/trafikverket.js
@@ -1,72 +1,79 @@
 import { getHoursMinutsFromTime } from "../../util/util.js";
 import { getStationNames, getStationByName } from "./preload.js";
-import {getAllStopsByTrainId} from "./events.js";
+import { getAllStopsByTrainId } from "./events.js";
 import "./preload.js";
 
-document.getElementById("showBtn").addEventListener("click", () => Search());
 
-$.support.cors = true; // Enable Cross domain requests
-try {
+function preDefind() {
+  $.support.cors = true; // Enable Cross domain requests
+  try {
     $.ajaxSetup({
-        //url: "http://localhost",
-        error: function(msg) {
-            if (msg.statusText === "abort") return;
-            alert("Request failed: " + msg.statusText + "\n" + msg.responseText);
-            console.log(msg);
-        }
+      //url: "http://localhost",
+      error: function (msg) {
+        if (msg.statusText === "abort") return;
+        alert("Request failed: " + msg.statusText + "\n" + msg.responseText);
+        console.log(msg);
+      },
     });
-} catch (e) {
+  } catch (e) {
     alert("Ett fel uppstod vid initialisering.");
-}
-// Create an ajax loading indicator
-let loadingTimer;
-$("#loader").hide();
-$(document).ajaxStart(function() {
-    loadingTimer = setTimeout(function() {
+  }
+  // Create an ajax loading indicator
+  let loadingTimer;
+  $("#loader").hide();
+  $(document)
+    .ajaxStart(function () {
+      loadingTimer = setTimeout(function () {
         $("#loader").show();
-    }, 200);
-}).ajaxStop(function() {
-    clearTimeout(loadingTimer);
-    $("#loader").hide();
-});
+      }, 200);
+    })
+    .ajaxStop(function () {
+      clearTimeout(loadingTimer);
+      $("#loader").hide();
+    });
+}
+preDefind();
+
+
 // Load stations
 
-
 function Search() {
-    let sign = $("#station").data("sign");
-    // Clear html table
-    $('#timeTableDeparture tr:not(:first)').remove();
+  let sign = $("#station").data("sign");
+  // Clear html table
+  $("#timeTableDeparture tr:not(:first)").remove();
 
-    // Request to load announcements for a station by its signature
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "http://localhost/trafikverket/departures/" + sign,
-        success: function(response) {
-            if (response == null) return;
-            if (response.RESPONSE.RESULT[0].TrainAnnouncement == null)
-                jQuery("#timeTableDeparture tr:last").
-            after('<tr class="list"><td colspan="4">Inga avgångar hittades</td></tr>');
-            try {
-                renderTrainAnnouncement(response.RESPONSE.RESULT[0].TrainAnnouncement);
-            } catch (ex) {}
-        }
-    });
+  // Request to load announcements for a station by its signature
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "http://localhost/trafikverket/departures/" + sign,
+    success: function (response) {
+      if (response == null) return;
+      if (response.RESPONSE.RESULT[0].TrainAnnouncement == null)
+        jQuery("#timeTableDeparture tr:last").after(
+          '<tr class="list"><td colspan="4">Inga avgångar hittades</td></tr>'
+        );
+      try {
+        renderTrainAnnouncement(response.RESPONSE.RESULT[0].TrainAnnouncement);
+      } catch (ex) {}
+    },
+  });
 }
 
 function renderTrainAnnouncement(announcement) {
-    announcement.forEach(item => {
-        const {hours, minutes} = getHoursMinutsFromTime(item.AdvertisedTimeAtLocation)
-        let depTime = (hours + ":" + minutes);
-        let toList = getStationNames(item.ToLocation);
-        
-        let owner = "";
-        if (item.InformationOwner != null) owner = item.InformationOwner;
-        
-        jQuery("#timeTableDeparture tr:last").
-        after(`<tr>
+  announcement.forEach((item) => {
+    const { hours, minutes } = getHoursMinutsFromTime(
+      item.AdvertisedTimeAtLocation
+    );
+    let depTime = hours + ":" + minutes;
+    let toList = getStationNames(item.ToLocation);
+
+    let owner = "";
+    if (item.InformationOwner != null) owner = item.InformationOwner;
+
+    jQuery("#timeTableDeparture tr:last").after(`<tr>
                 <td>${depTime}</td>
-                <td>${toList.join(', ')}</td>
+                <td>${toList.join(", ")}</td>
                 <td>${owner}</td>
                 <td style='text-align: center'>${item.TrackAtLocation}</td>
                 <td>${item.AdvertisedTrainIdent}</td>
@@ -76,27 +83,31 @@ function renderTrainAnnouncement(announcement) {
                 data-dep-time=${depTime} 
                 data-train-id=${item.AdvertisedTrainIdent} 
                 type='button' 
-                onclick="getTrainById(${item.AdvertisedTrainIdent})">Välj resa</button></td>
+                onclick="getTrainById(${
+                  item.AdvertisedTrainIdent
+                })">Välj resa</button></td>
             </tr>"
         `);
-    });
+  });
 }
 
 function getTrainById(trainIdent) {
-    let train = document.querySelector(`[data-train-id="${trainIdent}"]`);
-    let depTime = train.dataset["depTime"];
-    let trainId = train.dataset["data-train-id"];
-    let owner = train.dataset["owner"];
-    clearBox("main_content");
-    
-    getAllStopsByTrainId(trainIdent).then(result => {
-        result.forEach(item => {
-            const {hours, minutes} = getHoursMinutsFromTime(item.AdvertisedTimeAtLocation);
-            let depTime = (hours + ":" + minutes);
-            const stationName = getStationByName(item.LocationSignature);
+  let train = document.querySelector(`[data-train-id="${trainIdent}"]`);
+  let depTime = train.dataset["depTime"];
+  let trainId = train.dataset["data-train-id"];
+  let owner = train.dataset["owner"];
+  clearBox("main_content");
 
-            jQuery("#timeTableDeparture tr:last").
-            after(`<tr>
+  getAllStopsByTrainId(trainIdent)
+    .then((result) => {
+      result.forEach((item) => {
+        const { hours, minutes } = getHoursMinutsFromTime(
+          item.AdvertisedTimeAtLocation
+        );
+        let depTime = hours + ":" + minutes;
+        const stationName = getStationByName(item.LocationSignature);
+
+        jQuery("#timeTableDeparture tr:last").after(`<tr>
                     <td>${depTime}</td>
                     <td><button 
                     class="selectTrain"
@@ -104,9 +115,10 @@ function getTrainById(trainIdent) {
                     onclick="")">${stationName}</button></td>
                 </tr>"
             `);
-        });
-    }).catch(e => console.assert(e));
-    let stopsHTML = `
+      });
+    })
+    .catch((e) => console.assert(e));
+  let stopsHTML = `
         <main>
             <div id="searchbar">
                 <h3 class="pagetitle">Sök slut station</h3>
@@ -127,31 +139,29 @@ function getTrainById(trainIdent) {
         </main>
     `;
 
-    $("#main_content").append(stopsHTML);
+  $("#main_content").append(stopsHTML);
 }
 
 function clearBox(elementID) {
-    document.getElementById(elementID).innerHTML = "";
+  document.getElementById(elementID).innerHTML = "";
 }
-
-
 
 // http://jsfiddle.net/je01otqn/36/
 //let departureTime = document.getElementById("departureTime").value;
 //let arrivalTime = document.getElementById("arrivalTime").value;
 
 function calcTimeDiffrence(departureTime, arrivalTime) {
-    departureTime = departureTime.split(":");
-    arrivalTime = arrivalTime.split(":");
-    let startDate = new Date(0, 0, 0, departureTime[0], departureTime[1], 0);
-    let endDate = new Date(0, 0, 0, arrivalTime[0], arrivalTime[1], 0);
-    let diff = endDate.getTime() - startDate.getTime();
-    let hours = Math.floor(diff / 1000 / 60 / 60);
-    diff -= hours * 1000 * 60 * 60;
-    let minutes = Math.floor(diff / 1000 / 60);
-    let timeDiff = (hours < 9 ? "0" : "") + hours + ":" + (minutes < 9 ? "0" : "") + minutes;
-    console.log(timeDiff)
-
+  departureTime = departureTime.split(":");
+  arrivalTime = arrivalTime.split(":");
+  let startDate = new Date(0, 0, 0, departureTime[0], departureTime[1], 0);
+  let endDate = new Date(0, 0, 0, arrivalTime[0], arrivalTime[1], 0);
+  let diff = endDate.getTime() - startDate.getTime();
+  let hours = Math.floor(diff / 1000 / 60 / 60);
+  diff -= hours * 1000 * 60 * 60;
+  let minutes = Math.floor(diff / 1000 / 60);
+  let timeDiff =
+    (hours < 9 ? "0" : "") + hours + ":" + (minutes < 9 ? "0" : "") + minutes;
+  console.log(timeDiff);
 }
 //document.getElementById("timeDiffrence").value = calcTimeDiffrence(departureTime, arrivalTime);
 
@@ -161,4 +171,6 @@ function calcTimeDiffrence(departureTime, arrivalTime) {
 <input id="timeDiffrence">
 */
 
+// Export globaly from modul scope.
 window.getTrainById = getTrainById;
+window.Search = Search();
