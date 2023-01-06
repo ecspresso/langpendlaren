fd3ee81b0fe5eb52a2988f144edabe48c34bc81f
Pakdel <habib.pakdel1121@gmail.com>
2023-01-06 16:21:52 +0100

    wip

--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -1,44 +1,4 @@
 import "../logic/trafic/trafic_preload.js";
-function PreloadTrainStations() {
-  // Request to load all stations
-  var xmlRequest =
-    "<REQUEST>" +
-    // Use your valid authenticationkey
-    "<LOGIN authenticationkey='ab3f9656870c4625adfaaca4b76caeae'/>" +
-    "<QUERY objecttype='TrainStation' schemaversion='1'>" +
-    "<FILTER/>" +
-    "<INCLUDE>Prognosticated</INCLUDE>" +
-    "<INCLUDE>AdvertisedLocationName</INCLUDE>" +
-    "<INCLUDE>LocationSignature</INCLUDE>" +
-    "</QUERY>" +
-    "</REQUEST>";
-  $.ajax({
-    type: "POST",
-    contentType: "text/xml",
-    dataType: "json",
-    data: xmlRequest,
-    success: function (response) {
-      if (response == null) return;
-      try {
-        var stationlist = [];
-        $(response.RESPONSE.RESULT[0].TrainStation).each(function (
-          iterator,
-          item
-        ) {
-          // Save a key/value list of stations
-          Stations[item.LocationSignature] = item.AdvertisedLocationName;
-          // Create an array to fill the search field autocomplete.
-          if (item.Prognosticated == true)
-            stationlist.push({
-              label: item.AdvertisedLocationName,
-              value: item.LocationSignature,
-            });
-        });
-        return stationlist;
-      } catch (ex) {}
-    },
-  });
-}
 
 /**
  * Funktion för att formattera tiden.
@@ -74,4 +34,4 @@ function millisecondsToHoursAndMinutes(milliseconds){
   return hours + ":" + minutes;
 }
 
-export { PreloadTrainStations, getHoursMinutsFromTime, millisecondsToHoursAndMinutes };
+export { getHoursMinutsFromTime, millisecondsToHoursAndMinutes };
