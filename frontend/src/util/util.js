368df8424be3315d280e741a075605361a58ba48
Pakdel <habib.pakdel1121@gmail.com>
2023-01-06 16:19:09 +0100

    millisecondsToHoursAndMinutes, comments

--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -40,6 +40,11 @@ function PreloadTrainStations() {
   });
 }
 
+/**
+ * Funktion för att formattera tiden.
+ * @param {string} AdvertisedTimeAtLocation 
+ * @returns {string}
+ */
 function getHoursMinutsFromTime(AdvertisedTimeAtLocation) {
   let advertisedTime = new Date(AdvertisedTimeAtLocation);
   let hours = advertisedTime.getHours();
@@ -50,4 +55,23 @@ function getHoursMinutsFromTime(AdvertisedTimeAtLocation) {
   return { hours, minutes };
 }
 
-export { PreloadTrainStations, getHoursMinutsFromTime };
+/**
+ * Funktion för att göra tiden läsbar för användaren. Skrivs sedan ut i HTML koden
+ * @param {string|null} milliseconds 
+ * @returns {string}
+ */
+function millisecondsToHoursAndMinutes(milliseconds){
+  let seconds = milliseconds / 1000;
+  let minutes = seconds / 60;
+  let hours = Math.floor(minutes / 60);
+  minutes = Math.floor(minutes % 60);
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  return hours + ":" + minutes;
+}
+
+export { PreloadTrainStations, getHoursMinutsFromTime, millisecondsToHoursAndMinutes };
