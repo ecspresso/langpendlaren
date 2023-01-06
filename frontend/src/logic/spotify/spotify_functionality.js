a2df6abae2e81a8823f10dc0a45e01ee9371a8c2
Pakdel <habib.pakdel1121@gmail.com>
2023-01-06 18:06:10 +0100

    clean up

---
+++ frontend/src/logic/spotify/spotify_functionality.js
@@ -0,0 +1,15 @@
+
+
+
+function savePlayList(){
+    console.log("Save play list")
+}
+
+function newPlaylist() {
+    console.log("newPlaylist fungerar!");
+}
+
+
+
+
+export {savePlayList, newPlaylist}
