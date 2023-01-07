e4133e8f184ecf822cd794bcd417ca91fb5686a0
ecspresso <emile@ecspreso.com>
2023-01-07 23:45:16 +0100

    Rätt kod för .sh script installation av frontend.

--- frontend/install.sh
+++ frontend/install.sh
@@ -1,2 +1,3 @@
-cd ..
 electron-packager . frontend --overwrite --asar --platform=linux --arch=x64  --prune=true --out=release-builds
+electron-packager . --overwrite --platform=darwin --arch=x64 --prune=true --out=release-builds
+electron-packager . frontend --overwrite --asar --platform=win32 --arch=x64 --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="Frontend"
