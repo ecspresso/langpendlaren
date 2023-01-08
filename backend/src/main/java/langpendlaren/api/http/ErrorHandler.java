8c1c0cd68c1fca848d7f56c81933010d73f2f77e
ecspresso <emile@ecspreso.com>
2023-01-08 10:44:03 +0100

    Bättre namn på error message

--- backend/src/main/java/langpendlaren/api/http/ErrorHandler.java
+++ backend/src/main/java/langpendlaren/api/http/ErrorHandler.java
@@ -7,14 +7,21 @@ import langpendlaren.api.http.json.error.Error;
 public class ErrorHandler {
     /**
      * Short hand for internal server error.
-     * @param context Context handler send the reply to the client.
+     * @param context Context handler that sends the reply to the client.
      * @param exception Exception thrown.
      */
     public static void sendErrorMessage(Context context, Exception exception) {
         exception.printStackTrace();
         sendErrorMessage(context, exception, exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    
+
+    /**
+     * Creates error message that is sent to the client.
+     * @param context Context handler that sends the reply to the client.
+     * @param exception Exception thrown.
+     * @param message Message explaining the error in more detail.
+     * @param status HTTP status code.
+     */
     public static void sendErrorMessage(Context context, Exception exception, String message, HttpStatus status) {
         exception.printStackTrace();
         Error error = new Error();
