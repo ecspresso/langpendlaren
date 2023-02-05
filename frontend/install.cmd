956f65de9ef8af82abea23c75940a6fdaecc5146
Khabib <study@khabibs-mbp.mynet>
2023-02-05 12:11:36 +0100

    removed extra tools

--- frontend/install.cmd
+++
@@ -1,3 +0,0 @@
-call electron-packager . frontend --overwrite --asar --platform=linux --arch=x64  --prune=true --out=release-builds
-call electron-packager . --overwrite --platform=darwin --arch=x64 --prune=true --out=release-builds
-call electron-packager . frontend --overwrite --asar --platform=win32 --arch=x64 --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="Frontend"
